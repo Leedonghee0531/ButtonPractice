b5d0d59d0ec6e7bbe0792c89c3eed4238045b557
Leedonghee <heedong0531@naver.com>
2022-11-10 17:53:42 +0900

    Caculator practice

--- Button change practice/ViewController.swift
+++ Button change practice/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textid: UILabel!
+    
+    var leftValue: Int = 0
+    var rightValue: Int = 0
+    var isEditStarted = false
+    var type: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func numberClick(_ sender: UIButton) {
+        if !isEditStarted {
+            isEditStarted = true
+            self.textid.text = ""
+        }
+        self.textid.text = self.textid.text! + sender.titleLabel!.text!
+    }
+  
+  
+    @IBAction func result(_ sender: Any) {
+        rightValue = Int(textid.text!)!
+        var result = 0
+        if type == 0 {
+            result = leftValue + rightValue
+        } else if type == 1 {
+            result = leftValue - rightValue
+        } else if type == 2 {
+            result = leftValue * rightValue
+        } else if type == 3 {
+            result = leftValue / rightValue
+        }
+        textid.text = String(result)
+        leftValue = 0
+        rightValue = 0
+        isEditStarted = false
+    }
+    
+    @IBAction func curOperator(_ sender: UIButton) {
+        leftValue = Int(textid.text!)!
+        textid.text = ""
+        type = sender.tag
+    }
 }
-
